66cc5c354d8aa869ebf708494fe4f9868f200ae3
abudiayu <abudiayuu@gmail.com>
2025-12-18 00:21:44 +0300

    FOOTER

--- src/components/Pages/Landing/Landing.jsx
+++ src/components/Pages/Landing/Landing.jsx
@@ -3,13 +3,15 @@ import LayOut from '../../LayOut/LayOut';
 import CarouselEffect from '../../Carousel/CarouselEffect';
 import Category from '../../Category/Category';
 import Product from '../../Product/Product';
+import Footer from '../../Footer/Footer';
 
 function Landing() {
   return (
     <LayOut>
       <CarouselEffect/>
       <Category/>
-      <Product/>        
+      <Product/> 
+      <Footer/>       
     </LayOut>
   )
 }
