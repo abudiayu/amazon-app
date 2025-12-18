ef98a6155e9708c9afcfe30dc624a1200bee48d3
abudiayu <abudiayuu@gmail.com>
2025-12-18 16:11:11 +0300

    img added

--- src/components/Carousel/img/data.js
+++ src/components/Carousel/img/data.js
@@ -3,5 +3,7 @@ import img2 from "./10002.jpg";
 import img3 from "./img3.jpg";
 import img4 from "./img5.jpg";
 import img5 from "./immg6.jpg";
+import img6 from './img4.jpg';
+import img7 from "./img7.jpg";
 
-export const img = [img1,img2,img3,img4,img5];
+export const img = [img1,img2,img3,img4,img5,img6,img7];
