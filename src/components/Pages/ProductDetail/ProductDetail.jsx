894ed319a95251be95c4c75658abfef70d8e820f
abudiayu <abudiayuu@gmail.com>
2025-12-19 20:31:45 +0300

    Fix case-sensitive paths for CSS modules

--- src/components/Pages/ProductDetail/ProductDetail.jsx
+++ src/components/Pages/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import classes from './productDetail.module.css';
 import LayOut from '../../LayOut/LayOut';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
